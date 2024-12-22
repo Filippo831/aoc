09eb33cb6815a16720f5a93bf0cf8aa8c28ffbc7
Filippo831 <filippoarduini83@gmail.com>
2024-12-22 17:10:21 +0100

    2024 day_05 part1 another attempt

--- 2024/day_05/src/main.rs
+++ 2024/day_05/src/main.rs
@@ -1,57 +1,55 @@
-use std::fs;
+use std::{collections::HashMap, fs};
 
-fn part1(input_path: &str) -> i32 {
+fn part1(input_path: &str) -> u32 {
     let binding = fs::read_to_string(&input_path).unwrap();
     let first_read: Vec<&str> = binding.split("\n\n").collect();
 
-    let mut rules: Vec<(i32, i32)> = vec![];
+    let mut rules: HashMap<u32, u32> = HashMap::new();
     for line in first_read[0].split("\n").into_iter() {
-        let result: Vec<i32> = line.split("|").map(|e| e.parse::<i32>().unwrap()).collect();
-        rules.push((result[0], result[1]));
+        let result: Vec<u32> = line.split("|").map(|e| e.parse::<u32>().unwrap()).collect();
+        if let Some(value) = rules.get(&result[0]) {
+            rules.insert(result[0], value+1);
+        } else {
+            rules.insert(result[0], 1);
+        }
     }
 
-    let mut orders: Vec<Vec<i32>> = first_read[1].trim()
+    let mut orders: Vec<Vec<u32>> = first_read[1]
+        .trim()
         .split("\n")
         .map(|line| {
             line.split(",")
-                .map(|number| {
-                    number.parse::<i32>().unwrap()
-                })
+                .map(|number| number.parse::<u32>().unwrap())
                 .collect()
         })
         .collect();
 
-    let mut queue: Vec<i32> = vec![rules[0].0, rules[0].1];
+    let mut sum: u32 = 0;
 
-    while true {
-        for index in 0..rules.len() {
-            if index == rules.len() {
-                break
+    for order in &orders {
+        let mut ordered_list: Vec<u32> = vec![];
+        for el in order {
+            match rules.get(&el) {
+                Some(value) => ordered_list.push(*value),
+                None => ordered_list.push(0),
             }
-            let position = queue.iter().position(|&e| rules[index].1 == e);
-            match position {
-                Some(position) => {
-                    queue.insert(position, rules[index].0);
-                    rules.remove(index);
-                    println!("{}", position);
-                }
-                None => {}
-            };
         }
-        if rules.len() == 0 {
-            break
+
+        let mut correct = true;
+        for el in ordered_list.windows(2) {
+            if el[0] < el[1] {
+                correct = false;
+            }
+        }
+        if correct {
+            sum = sum + order[&order.len()/2]
         }
     }
-
-    for el in queue {
-        println!("{}", el);
-    }
-
-    return 0;
+    return sum;
 }
 
 fn main() {
-    part1("input_test.txt");
+    println!("{}", part1("input.txt"));
 }
 
 #[cfg(test)]
@@ -60,7 +58,7 @@ mod tests {
 
     #[test]
     fn test_part1() {
-        let result: i32 = part1("input_test.txt");
+        let result: u32 = part1("input_test.txt");
         assert_eq!(result, 143)
     }
 }
