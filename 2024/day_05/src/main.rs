340655fba1022e89d24ecc3cb8ba2fff1a2f7f68
Filippo831 <filippoarduini83@gmail.com>
2024-12-30 11:29:52 +0100

    2024 day_05 part1

--- 2024/day_05/src/main.rs
+++ 2024/day_05/src/main.rs
@@ -1,16 +1,21 @@
-use std::{collections::HashMap, fs};
+use std::{
+    collections::{HashMap, HashSet},
+    fs,
+};
 
 fn part1(input_path: &str) -> u32 {
     let binding = fs::read_to_string(&input_path).unwrap();
     let first_read: Vec<&str> = binding.split("\n\n").collect();
 
-    let mut rules: HashMap<u32, u32> = HashMap::new();
+    let mut rules: HashMap<u32, HashSet<u32>> = HashMap::new();
     for line in first_read[0].split("\n").into_iter() {
         let result: Vec<u32> = line.split("|").map(|e| e.parse::<u32>().unwrap()).collect();
         if let Some(value) = rules.get(&result[0]) {
-            rules.insert(result[0], value+1);
+            let mut hash_map_copy = value.clone();
+            hash_map_copy.insert(result[1]);
+            rules.insert(result[0], hash_map_copy);
         } else {
-            rules.insert(result[0], 1);
+            rules.insert(result[0], HashSet::from([result[1]]));
         }
     }
 
@@ -27,29 +32,79 @@ fn part1(input_path: &str) -> u32 {
     let mut sum: u32 = 0;
 
     for order in &orders {
-        let mut ordered_list: Vec<u32> = vec![];
-        for el in order {
-            match rules.get(&el) {
-                Some(value) => ordered_list.push(*value),
-                None => ordered_list.push(0),
+        let mut is_good: bool = true;
+        for (el_index, el) in order.iter().enumerate() {
+            match rules.get(el) {
+                Some(rules_value) => {
+                    for index in 0..el_index {
+                        match rules_value.get(&order[index]) {
+                            Some(value) => is_good = false,
+                            None => {}
+                        }
+                    }
+                }
+                None => {}
             }
         }
-
-        let mut correct = true;
-        for el in ordered_list.windows(2) {
-            if el[0] < el[1] {
-                correct = false;
-            }
-        }
-        if correct {
+        if is_good{
             sum = sum + order[&order.len()/2]
         }
     }
     return sum;
 }
 
+fn part2(input_path: &str) -> u32 {
+    let binding = fs::read_to_string(&input_path).unwrap();
+    let first_read: Vec<&str> = binding.split("\n\n").collect();
+
+    let mut rules: HashMap<u32, HashSet<u32>> = HashMap::new();
+    for line in first_read[0].split("\n").into_iter() {
+        let result: Vec<u32> = line.split("|").map(|e| e.parse::<u32>().unwrap()).collect();
+        if let Some(value) = rules.get(&result[0]) {
+            let mut hash_map_copy = value.clone();
+            hash_map_copy.insert(result[1]);
+            rules.insert(result[0], hash_map_copy);
+        } else {
+            rules.insert(result[0], HashSet::from([result[1]]));
+        }
+    }
+
+    let mut orders: Vec<Vec<u32>> = first_read[1]
+        .trim()
+        .split("\n")
+        .map(|line| {
+            line.split(",")
+                .map(|number| number.parse::<u32>().unwrap())
+                .collect()
+        })
+        .collect();
+
+    let mut sum: u32 = 0;
+
+    for order in &orders {
+        let mut is_good: bool = true;
+        for (el_index, el) in order.iter().enumerate() {
+            match rules.get(el) {
+                Some(rules_value) => {
+                    for index in 0..el_index {
+                        match rules_value.get(&order[index]) {
+                            Some(value) => is_good = false,
+                            None => {}
+                        }
+                    }
+                }
+                None => {}
+            }
+        }
+        if is_good{
+            sum = sum + order[&order.len()/2]
+        }
+    }
+    return sum;
+}
 fn main() {
-    println!("{}", part1("input.txt"));
+    println!("part1: {}", part1("input.txt"));
+    println!("part2: {}", part2("input.txt"));
 }
 
 #[cfg(test)]
@@ -60,5 +115,11 @@ mod tests {
     fn test_part1() {
         let result: u32 = part1("input_test.txt");
         assert_eq!(result, 143)
+    }
+
+    #[test]
+    fn test_part2() {
+        let result: u32 = part2("input_test.txt");
+        assert_eq!(result, 123)
     }
 }
