b631715bf5a0c922e8a5825059c7b2da3f7f5bdd
Filippo831 <filippoarduini83@gmail.com>
2025-01-01 09:51:33 +0100

    2024 day_07 part2

--- 2024/day_07/src/main.rs
+++ 2024/day_07/src/main.rs
@@ -1,6 +1,6 @@
 use std::fs;
 
-fn recursion(
+fn recursion_part1(
     result: i64,
     operands: &Vec<i64>,
     index: usize,
@@ -28,9 +28,9 @@ fn recursion(
         }
     }
     boolean_output =
-        boolean_output | recursion(result, operands, index + 1, temp_internal_value, true);
+        boolean_output | recursion_part1(result, operands, index + 1, temp_internal_value, true);
     boolean_output =
-        boolean_output | recursion(result, operands, index + 1, temp_internal_value, false);
+        boolean_output | recursion_part1(result, operands, index + 1, temp_internal_value, false);
 
     return boolean_output;
 }
@@ -52,16 +52,78 @@ fn part1(input: &str) -> i64 {
     let mut total_sum: i64 = 0;
 
     for el in parameters {
-        if recursion(el.0, &el.1, 0, 0, true) {
+        if recursion_part1(el.0, &el.1, 0, 0, true) {
             total_sum += el.0;
         }
     }
     return total_sum;
 }
 
+fn recursion_part2(
+    result: u64,
+    operands: &Vec<u64>,
+    index: usize,
+    internal_value: u64,
+    this_operand: u8,
+) -> bool {
+    let mut boolean_output: bool = false;
+    let mut temp_internal_value = internal_value;
+
+    if this_operand == 0 {
+        temp_internal_value = temp_internal_value + operands[index];
+    } else if this_operand == 1{
+        temp_internal_value = temp_internal_value * operands[index];
+    } else {
+        temp_internal_value = format!("{}{}", temp_internal_value, operands[index]).parse::<u64>().unwrap();
+    }
+
+    if internal_value > result {
+        return false;
+    }
+
+    if index == operands.len() - 1 {
+        if result == temp_internal_value {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    boolean_output =
+        boolean_output | recursion_part2(result, operands, index + 1, temp_internal_value, 0);
+    boolean_output =
+        boolean_output | recursion_part2(result, operands, index + 1, temp_internal_value, 1);
+    boolean_output =
+        boolean_output | recursion_part2(result, operands, index + 1, temp_internal_value, 2);
+
+    return boolean_output;
+}
+
+fn part2(input: &str) -> u64 {
+    let mut parameters: Vec<(u64, Vec<u64>)> = vec![];
+
+    for line in fs::read_to_string(input).unwrap().lines() {
+        let parts: Vec<&str> = line.split(": ").collect();
+        let result_sum = parts[0].parse::<u64>().unwrap();
+        let operands = parts[1]
+            .split(" ")
+            .map(|e| e.parse::<u64>().unwrap())
+            .collect();
+
+        parameters.push((result_sum, operands));
+    }
+
+    let mut total_sum: u64 = 0;
+
+    for el in parameters {
+        if recursion_part2(el.0, &el.1, 0, 0, 0) {
+            total_sum += el.0;
+        }
+    }
+    return total_sum;
+}
 fn main() {
-    let result = part1("input.txt");
-    println!("part1 result: {}", result);
+    println!("part1 result: {}", part1("input.txt"));
+    println!("part2 result: {}", part2("input.txt"));
 }
 
 #[cfg(test)]
@@ -71,5 +133,9 @@ mod tests {
     #[test]
     fn test_part1() {
         assert_eq!(part1("input_test.txt"), 3749);
+    }
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2("input_test.txt"), 11387);
     }
 }
