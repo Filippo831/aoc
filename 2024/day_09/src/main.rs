997255134ec24b2deacbd56c2d5ce4c2146d8327
Filippo831 <filippoarduini83@gmail.com>
2025-01-03 09:04:13 +0100

    2024 day_09 part2, not complete

--- 2024/day_09/src/main.rs
+++ 2024/day_09/src/main.rs
@@ -1,4 +1,4 @@
-use std::{fs, result};
+use std::fs;
 
 fn part1(input: &str) -> i64 {
     let mut fixed_string: Vec<i32> = vec![];
@@ -48,8 +48,61 @@ fn part1(input: &str) -> i64 {
     return output;
 }
 
+fn part2(input: &str) -> i64 {
+    // let mut fixed_string: Vec<i32> = vec![];
+    let mut numbers: Vec<(i32, u32)> = vec![];
+    let mut is_file = true;
+    for (index, c) in fs::read_to_string(input)
+        .unwrap()
+        .trim()
+        .chars()
+        .enumerate()
+    {
+        if is_file {
+            numbers.push((((index / 2) as i32), c.to_digit(10).unwrap()));
+        } else {
+            numbers.push((-1, c.to_digit(10).unwrap()));
+        }
+        is_file = !is_file;
+    }
+
+    let mut result_vec: Vec<i32> = vec![];
+
+    let mut backward_index: usize = numbers.len() - 1;
+
+    while backward_index >= 0 {
+        let backward_element = numbers[backward_index];
+        if backward_element.0 == -1 {
+            let mut forward_index: usize = 0;
+            while forward_index < numbers.len() {
+                let mut forward_element = numbers[forward_index];
+                if forward_element.0 == -1 && forward_element.1 >= backward_element.1 {
+                    numbers.remove(backward_index);
+                    numbers.insert(forward_index, backward_element);
+                    forward_element.1 -= backward_element.1;
+
+                    numbers.remove(forward_index+1);
+                    numbers.insert(forward_index+1, forward_element);
+                }
+                forward_index += 1;
+            }
+        }
+        if backward_index == 0 {
+            break
+        }
+        backward_index -= 1;
+    }
+
+    dbg!("{:?}", numbers);
+    let mut output: i64 = 0;
+    for (index, el) in result_vec.iter().enumerate() {
+        output += (index as i32 * *el) as i64
+    }
+    return output;
+}
 fn main() {
-    println!("{}", part1("input.txt"));
+    println!("part1: {}", part1("input.txt"));
+    println!("part2: {}", part2("input.txt"));
 }
 
 #[cfg(test)]
@@ -59,5 +112,10 @@ mod tests {
     #[test]
     fn test_part1() {
         assert_eq!(part1("input_test.txt"), 1928);
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2("input_test.txt"), 2858);
     }
 }
