1c932609c2033b2479aa011ad4a5f383926d7030
Filippo831 <filippoarduini83@gmail.com>
2025-12-05 08:27:00 +0100

    2025 day05 not done yet

---
+++ 2025/day_05/src/main.rs
@@ -0,0 +1,109 @@
+use std::{fs, vec};
+
+fn part1(input: &str) -> u64 {
+    let binding = fs::read_to_string(input).unwrap();
+    let content: Vec<&str> = binding.split("\n\n").collect();
+
+    let rules: Vec<(u64, u64)> = content[0]
+        .lines()
+        .map(|line| {
+            (
+                line.split("-").nth(0).unwrap().parse::<u64>().unwrap(),
+                line.split("-").nth(1).unwrap().parse::<u64>().unwrap(),
+            )
+        })
+        .collect();
+
+    let cases: Vec<u64> = content[1]
+        .lines()
+        .map(|e| e.parse::<u64>().unwrap())
+        .collect();
+
+    let mut goods: u64 = 0;
+    for element in cases {
+        for index in 0..rules.len() {
+            if element <= rules[index].1 && element >= rules[index].0 {
+                goods = goods + 1;
+                break;
+            }
+        }
+    }
+    return goods;
+}
+
+fn part2(input: &str) -> u64 {
+    let binding = fs::read_to_string(input).unwrap();
+    let content: Vec<&str> = binding.split("\n\n").collect();
+
+    let mut rules: Vec<(u64, u64)> = content[0]
+        .lines()
+        .map(|line| {
+            (
+                line.split("-").nth(0).unwrap().parse::<u64>().unwrap(),
+                line.split("-").nth(1).unwrap().parse::<u64>().unwrap(),
+            )
+        })
+        .collect();
+
+    for index_first in 0..rules.len() {
+        if index_first >= rules.len() {
+            break;
+        }
+        let mut low = rules[index_first].0;
+        let mut high = rules[index_first].1;
+        dbg!("{}", &rules);
+
+        for index_second in 0..rules.len() {
+            if index_second >= rules.len() {
+                break;
+            }
+            if rules[index_second].0 <= low
+                && rules[index_second].1 <= high
+                && rules[index_second].1 >= low
+            {
+                low = rules[index_second].0;
+                rules.remove(index_second);
+            } else if rules[index_second].1 >= high
+                && rules[index_second].0 <= high
+                && rules[index_second].0 >= low
+            {
+                high = rules[index_second].1;
+                rules.remove(index_second);
+            } else if rules[index_second].1 >= high && rules[index_second].0 <= low {
+                low = rules[index_second].0;
+                high = rules[index_second].1;
+                rules.remove(index_second);
+            }
+        }
+        rules[index_first].0 = low;
+        rules[index_first].1 = high;
+    }
+    let mut total: u64 = 0;
+    for r in rules {
+        total = total + r.1 - r.0;
+    }
+
+    return total;
+}
+
+fn main() {
+    let result_part1 = part1("input.txt");
+    println!("{}", result_part1);
+    let result_part2 = part2("input.txt");
+    println!("{}", result_part2);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(part1("input_test.txt"), 3);
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2("input_test.txt"), 14);
+    }
+}
