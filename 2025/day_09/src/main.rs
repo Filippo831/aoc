a12dd16007c6013c8ccf74fb704b510b912b28e1
Filippo831 <filippoarduini83@gmail.com>
2025-12-22 08:48:20 +0100

    2025 day 09, start part 2

--- 2025/day_09/src/main.rs
+++ 2025/day_09/src/main.rs
@@ -9,7 +9,8 @@ fn part1(input: &str) -> u64 {
     let mut largest: u64 = 0;
     for first_index in 0..content.len() {
         for second_index in first_index..content.len() {
-            let calculation = (content[first_index][0].abs_diff(content[second_index][0]) + 1) as u64
+            let calculation = (content[first_index][0].abs_diff(content[second_index][0]) + 1)
+                as u64
                 * (content[first_index][1].abs_diff(content[second_index][1]) + 1) as u64;
             if calculation > largest {
                 largest = calculation;
@@ -21,7 +22,34 @@ fn part1(input: &str) -> u64 {
 }
 
 fn part2(input: &str) -> u64 {
-    return 0;
+    let content: Vec<(i64, i64)> = fs::read_to_string(input)
+        .unwrap()
+        .lines()
+        .map(|e| {
+            let (a, b) = e.split_once(',').unwrap();
+            (a.parse::<i64>().unwrap(), b.parse::<i64>().unwrap())
+        })
+        .collect();
+
+    let mut largest: u64 = 0;
+
+    for index_first in 0..content.len() {
+        for index_second in index_first..content.len() {
+            let temp_largest: u64 = ((content[index_first].0 - content[index_second].0).abs()
+                * (content[index_first].1 - content[index_second].1).abs())
+                as u64;
+
+            if temp_largest < largest {
+                break;
+            } else {
+                let mut corners: [(i64, i64); 4] = [(0,0); 4];
+                corners[0] = content[index_first];
+
+            }
+        }
+    }
+
+    return largest;
 }
 
 fn main() {
@@ -42,6 +70,6 @@ mod tests {
 
     #[test]
     fn test_part2() {
-        assert_eq!(part2("input_test.txt"), 3263827);
+        assert_eq!(part2("input_test.txt"), 24);
     }
 }
